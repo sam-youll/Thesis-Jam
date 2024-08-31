4135233371e1941af41622468a831a194f2ec60d
sam-youll <sf4112@nyu.edu>
2024-08-31 16:18:15 -0400

    sdthgyjukyiluh.okgjhyhtgrhfyj

--- Scripts/conductor.gd
+++ Scripts/conductor.gd
@@ -1,8 +1,31 @@
 extends Node
 @onready var bank_loader: FmodBankLoader = $FmodBankLoader
 @onready var event_emitter: FmodEventEmitter2D = $FmodEventEmitter2D
+@onready var label: Label = $Label
 
+enum Turn 
+{
+	PLAYER_ONE,
+	PLAYER_TWO
+}
+var turn: Turn = Turn.PLAYER_ONE
 
+var beat: float = 0
+var timer: float = 0
+
+# array of floats showing time since measure start for each beat
+# e.g. index 1 is the first beat, which happened at 1.12
+var beatmap = []
+
+# returns time difference between hit and closest beat in beatmap
+func _check_hit(hit_time: float) -> float:
+	var num_beats = beatmap.size()
+	var beatmap_index_to_check = 0
+	for i in num_beats - 1:
+		if hit_time > beatmap[i] + (beatmap[i+1]-beatmap[i])/2:
+			beatmap_index_to_check += 1
+	return abs(beatmap[beatmap_index_to_check]-hit_time)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,5 +33,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("hit"):
+		_do_hit()
 	# use event_emitter signals to do conductory stuff
+	
+	match turn:
+		Turn.PLAYER_ONE:
+			label.text = "PLAYER ONE"
+		Turn.PLAYER_TWO:
+			label.text = "PLAYER TWO"
+	
+func _on_timeline_beat() -> void:
+	pass
+	
+func _do_hit() -> void:
+	event_emitter.event_guid = "rlgkjbnwg"
+	event_emitter.play()
--- Scripts/gerald.gd
+++ Scripts/gerald.gd
@@ -1,5 +1,12 @@
 extends Sprite2D
 var my_scale: float = 1
+var timer: float = 0
+var timer2: float = 0
+var bpm: int = 120
+var mimic: bool = false
+@onready var beat_label: Label = $"../BeatLabel"
+
+var beatmap = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +17,19 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("hit") and get_scale() == Vector2(1,1):
 		my_scale = 2
+		beatmap.append(timer)
+		timer = 0
+		
+	timer += delta
+	timer2 += delta
+	
+	if timer2/60*bpm >= 8:
+		timer2 = 0
 	
 	if my_scale > 1:
 		my_scale -= .1
 		
 	scale = (Vector2(my_scale, my_scale))
+	
+		
+	beat_label.text = str(int(timer2/60*bpm)+1) + "/8"
