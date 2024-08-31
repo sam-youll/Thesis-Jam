a264bf2cc630d69f8735552ade075b7605126242
sam-youll <sf4112@nyu.edu>
2024-08-31 14:12:51 -0400

    there's a gerald now

---
+++ Scripts/gerald.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+var my_scale: float = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("hit") and get_scale() == Vector2(1,1):
+		my_scale = 2
+	
+	if my_scale > 1:
+		my_scale -= .1
+		
+	scale = (Vector2(my_scale, my_scale))
