877052ccfe4ec5988a1d393624fb78867e5419d5
sam-youll <sf4112@nyu.edu>
2024-09-01 01:24:59 -0400

    you can record and playback recorded stuff
    press r to start record mode, which records all inputs for 4 bars
    
    then press space to go to playback mode
    
    it's buggy though

--- Scripts/conductor.gd
+++ Scripts/conductor.gd
@@ -1,7 +1,10 @@
 extends Node
-@onready var bank_loader: FmodBankLoader = $FmodBankLoader
-@onready var event_emitter: FmodEventEmitter2D = $FmodEventEmitter2D
 @onready var label: Label = $Label
+@onready var beat_label: Label = $BeatLabel
+@onready var event_emitter: FmodEventEmitter2D = $PlayerOneEventEmitter
+@onready var backing_track_emitter: FmodEventEmitter2D = $BackingTrackEmitter
+@onready var timer: Timer = $BeatTimer
+@onready var transition_timer: Timer = $TransitionTimer
 
 enum Turn 
 {
@@ -10,25 +13,34 @@ enum Turn
 }
 var turn: Turn = Turn.PLAYER_ONE
 
-var beat: float = 0
-var timer: float = 0
+enum State
+{
+	NONE,
+	TEST,
+	GOTORECORD,
+	RECORD,
+	READY,
+	GOTOPLAYBACK,
+	PLAYBACK
+}
+var state: State = State.NONE
 
+var beat: int = 0
+var beat_time: float = 0
+var recording_timer: float = 0
+var beats_left: int = 16
+
+var test_beatmap = [.5,1,.25,.25,.5,1]
+var beatmap_index = 0
 # array of floats showing time since measure start for each beat
 # e.g. index 1 is the first beat, which happened at 1.12
 var beatmap = []
 
-# returns time difference between hit and closest beat in beatmap
-func _check_hit(hit_time: float) -> float:
-	var num_beats = beatmap.size()
-	var beatmap_index_to_check = 0
-	for i in num_beats - 1:
-		if hit_time > beatmap[i] + (beatmap[i+1]-beatmap[i])/2:
-			beatmap_index_to_check += 1
-	return abs(beatmap[beatmap_index_to_check]-hit_time)
-	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	backing_track_emitter.play()
+	#spawn_circle()
+	#timer.start(test_beatmap[beatmap_index])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,15 +49,109 @@ func _process(delta: float) -> void:
 		_do_hit()
 	# use event_emitter signals to do conductory stuff
 	
+	beat_time += delta * 2
+	
 	match turn:
 		Turn.PLAYER_ONE:
-			label.text = "PLAYER ONE"
+			label.set_text("PLAYER ONE")
 		Turn.PLAYER_TWO:
-			label.text = "PLAYER TWO"
+			label.set_text("PLAYER TWO")
+			
+	if Input.is_action_just_pressed("record"):
+		beatmap = []
+		beatmap_index = 0
+		beats_left = 16
+		timer.stop()
+		transition_timer.stop()
+		state = State.GOTORECORD
+		
+	print(state)
 	
-func _on_timeline_beat() -> void:
-	pass
-	
+	match state:
+		State.NONE:
+			pass
+			
+		State.TEST:
+			pass
+		State.GOTORECORD:
+			pass
+		State.RECORD:
+			beat_label.set_text(str(beats_left))
+			recording_timer += delta
+			if Input.is_action_just_pressed("hit"):
+				beatmap.append(recording_timer)
+				recording_timer = 0
+				
+			if beats_left <= 0:
+				state = State.READY
+				
+		State.READY:
+			beat_label.set_text("press space to continue")
+			if Input.is_action_just_pressed("hit"):
+				state = State.GOTOPLAYBACK
+				
+		State.GOTOPLAYBACK:
+			pass
+			
+		State.PLAYBACK:
+			beat_label.set_text(str(beat))
+			pass
+			
+
 func _do_hit() -> void:
-	event_emitter.event_guid = "rlgkjbnwg"
 	event_emitter.play()
+	
+
+func _on_backing_track_emitter_timeline_beat(params: Dictionary) -> void:
+	#print("beat" + str(params.beat))
+	beat = params.beat
+	#spawn_circle()
+	beat_time = 0
+	
+	match state:
+		State.GOTORECORD:
+			if beat == 1:
+				state = State.RECORD
+		State.RECORD:
+			beats_left -= 1
+		State.GOTOPLAYBACK:
+			if beat == 1:
+				#for beat in beatmap:
+					#beat = quantize(beat)
+				state = State.PLAYBACK
+				timer.start(beatmap[beatmap_index])
+
+func _on_backing_track_emitter_started() -> void:
+	print("started")
+
+func spawn_circle():
+	var circle_scene = load("res://Scenes/circle.tscn")
+	var circle = circle_scene.instantiate()
+	add_child(circle)
+	
+func quantize(val: float) -> float:
+	return .125 * round(val * 8)
+	
+# returns time difference between hit and closest beat in beatmap
+func _check_hit(hit_time: float) -> float:
+	var num_beats = beatmap.size()
+	var beatmap_index_to_check = 0
+	for i in num_beats - 1:
+		if hit_time > beatmap[i] + (beatmap[i+1]-beatmap[i])/2:
+			beatmap_index_to_check += 1
+	return abs(beatmap[beatmap_index_to_check]-hit_time)
+
+
+func _on_timer_timeout() -> void:
+	beatmap_index += 1
+	if beatmap_index < beatmap.size():
+		spawn_circle()
+		timer.start(beatmap[beatmap_index])
+	else:
+		print("beatmap empty")
+		if state == State.RECORD and transition_timer.is_stopped():
+			transition_timer.start(4)
+
+
+func _on_transition_timer_timeout() -> void:
+	state = State.NONE
